8110e4734908bc89bc401ccb50ba8e9820d9b6c0
benrmartins <benrmartins@gmail.com>
2023-03-22 15:32:07 -0400

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,113 @@
+import dsa.Inversions;
+import dsa.LinkedQueue;
+import stdlib.In;
+import stdlib.StdOut;
+
+// A data type to represent a board in the 8-puzzle game or its generalizations.
+public class Board {
+    ...
+
+    // Constructs a board from an n x n array; tiles[i][j] is the tile at row i and column j, with 0
+    // denoting the blank tile.
+    public Board(int[][] tiles) {
+        ...
+    }
+
+    // Returns the size of this board.
+    public int size() {
+        ...
+    }
+
+    // Returns the tile at row i and column j of this board.
+    public int tileAt(int i, int j) {
+        ...
+    }
+
+    // Returns Hamming distance between this board and the goal board.
+    public int hamming() {
+        ...
+    }
+
+    // Returns the Manhattan distance between this board and the goal board.
+    public int manhattan() {
+        ...
+    }
+
+    // Returns true if this board is the goal board, and false otherwise.
+    public boolean isGoal() {
+        ...
+    }
+
+    // Returns true if this board is solvable, and false otherwise.
+    public boolean isSolvable() {
+        ...
+    }
+
+    // Returns an iterable object containing the neighboring boards of this board.
+    public Iterable<Board> neighbors() {
+        ...
+    }
+
+    // Returns true if this board is the same as other, and false otherwise.
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+        ...
+    }
+
+    // Returns a string representation of this board.
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2s", tiles[i][j] == 0 ? " " : tiles[i][j]));
+                if (j < n - 1) {
+                    s.append(" ");
+                }
+            }
+            if (i < n - 1) {
+                s.append("\n");
+            }
+        }
+        return s.toString();
+    }
+
+    // Returns a defensive copy of tiles[][].
+    private int[][] cloneTiles() {
+        int[][] clone = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                clone[i][j] = tiles[i][j];
+            }
+        }
+        return clone;
+    }
+
+    // Unit tests the data type. [DO NOT EDIT]
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tiles[i][j] = in.readInt();
+            }
+        }
+        Board board = new Board(tiles);
+        StdOut.printf("The board (%d-puzzle):\n%s\n", n, board);
+        String f = "Hamming = %d, Manhattan = %d, Goal? %s, Solvable? %s\n";
+        StdOut.printf(f, board.hamming(), board.manhattan(), board.isGoal(), board.isSolvable());
+        StdOut.println("Neighboring boards:");
+        for (Board neighbor : board.neighbors()) {
+            StdOut.println(neighbor);
+            StdOut.println("----------");
+        }
+    }
+}
